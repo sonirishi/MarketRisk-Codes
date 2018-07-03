468d1bf16f195809aeec69841ad724c97aed8a74
sonirishi <rishabhsoni.iit@gmail.com>
2018-07-04 00:42:53 +0530

    Add files via upload

--- numerical_opt.R
+++ numerical_opt.R
@@ -14,16 +14,25 @@ pracma::newtonRaphson(yield, x0 = 0.001)  ## not sure why result is different
 
 ########### Personal Newton Raphson ###########
 
-newton_method <- function(func,start = 0.001,maxiter=100,tol = 1.490116e-08){
-  val = func(start)
+yield <- function(y,mp,coupon,final,yrs){
+  cashflow = 0
+  for(i in 1:yrs){
+    cashflow = cashflow + coupon/(1+y)^i
+  }
+  cashflow = cashflow + final/(1+y)^i
+  return(mp - cashflow)
+}
+
+newton_method <- function(func,par,start = 0.001,maxiter=100,tol = 1.490116e-08){
+  val = func(start,par[1],par[2],par[3],par[4])
   if(val >= -tol & val <= tol){
     return(c(val,0,start))
   }else{
     x = start
     i = 1
     while(i <= maxiter){
-      x = x - func(x)/(grad(func,x) + 1e-12)   ## avoid division by 0
-      val = func(x)
+      x = x - func(x,par[1],par[2],par[3],par[4])/(grad(func,x,mp=par[1],coupon=par[2],final=par[3],yrs=par[4]) + 1e-12)   ## avoid division by 0
+      val = func(x,par[1],par[2],par[3],par[4])
       if(val >= -tol & val <= tol){
         return(c(val,i,x))
       }else{
@@ -34,4 +43,4 @@ newton_method <- function(func,start = 0.001,maxiter=100,tol = 1.490116e-08){
   return(c(func(val),"NoConvergence","None"))
 }  
 
-newton_method(yield)
+newton_method(yield,par=c(92,5,100,4))
